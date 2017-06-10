168d30452282cfb7403e04f543d33db4f6a50ce1
Dustin Essington <aetaric@gmail.com>
2017-06-10 16:22:31 -0400

    Added inflector methods to Channel class

--- lib/channel.rb
+++ lib/channel.rb
@@ -1,12 +1,38 @@
 class Channel
-  attr_accessor :name, :id, :game, :status, :live, :viewcount
+  attr_accessor :name, :id, :game, :status, :live, :viewcount, :host
 
   def go_live
     @live = true
+    @host = false
   end
 
   def go_offline
     @live = false
+    @host = true
+  end
+
+  def to_hash
+    hash = {}
+    hash["name"]      = @name
+    hash["id"]        = @id
+    hash["game"]      = @game
+    hash["status"]    = @status
+    hash["live"]      = @live
+    hash["viewcount"] = @viewcount
+    hash["host"]      = @host
+    return hash
+  end
+
+  def to_json
+    return to_hash.to_json
+  end
+
+  def to_s
+    return @name
+  end
+
+  def to_i
+    return @id
   end
 
 end
