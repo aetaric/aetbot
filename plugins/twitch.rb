0363e7adc8a8b4457f6e5acc4b046288c2a77552
Dustin Essington <aetaric@ecs-server.net>
2017-08-01 00:38:31 -0400

    implement changes for game and title changing

--- plugins/twitch.rb
+++ plugins/twitch.rb
@@ -15,7 +15,9 @@ class Twitch
   match /source/, method: :source
   match /host (.+)/, method: :host
   match /synthesis/, method: :synthesis
-  match /commands/, methods: :commands
+  match /commands/, method: :commands
+  match /game (.+)/, method: :game
+  match /title (.+)/, mehtod: :title
 
   def follow(m, plug)
     if mod?(m)
@@ -41,6 +43,64 @@ class Twitch
   def commands(m)
     if mod?(m)
       m.reply ".w #{m.user} My Commands are here: https://gist.githubusercontent.com/aetaric/df04e55c159baabafc2194f8516715fc/raw/e77e5db52718db1ca48cea64f3ccdb3c1c59e13f/gistfile1.txt"
+    end
+  end
+
+  def game(m, gamename)
+    if mod?(m)
+      @collection = $mongo[:users]
+      user = @collection.find_by :name => chan_to_user(m)
+
+      uri = URI.parse("https://api.twitch.tv/kraken/channels/" + user["uid"])
+      request = Net::HTTP::Put.new(uri)
+      request.content_type = "application/json"
+      request["Client-Id"] = $brain.twitch["client"]
+      request["Accept"] = "application/vnd.twitchtv.v5+json"
+      request["Authorization"] = "OAuth " + user["oauth"]
+      request.body = JSON.dump({
+        "channel" => {
+          "game" => gamename
+        }
+      })
+
+      req_options = {
+        use_ssl: uri.scheme == "https",
+      }
+
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+
+      m.reply "I've set the title to #{gamename}"
+    end
+  end
+
+  def title(m, title_string)
+    if mod?(m)
+      @collection = $mongo[:users]
+      user = @collection.find_by :name => chan_to_user(m)
+
+      uri = URI.parse("https://api.twitch.tv/kraken/channels/" + user["uid"])
+      request = Net::HTTP::Put.new(uri)
+      request.content_type = "application/json"
+      request["Client-Id"] = $brain.twitch["client"]
+      request["Accept"] = "application/vnd.twitchtv.v5+json"
+      request["Authorization"] = "OAuth " + user["oauth"]
+      request.body = JSON.dump({
+        "channel" => {
+          "status" => title_string
+        }
+      })
+
+      req_options = {
+        use_ssl: uri.scheme == "https",
+      }
+
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+
+      m.reply "I've set the title to #{title_string}"
     end
   end
 end
